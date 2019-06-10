1a4b6b4f5c2c2071d1721e7e505365e6aac70ace
pu1et <46569798+pu1et@users.noreply.github.com>
2019-06-11 02:35:07 +0900

    Update cvehunter.py

--- src/modules/hunting/cvehunter.py
+++ src/modules/hunting/cvehunter.py
@@ -9,7 +9,7 @@ from ...core.events import handler
 from ...core.events.common import Vulnerability, Event
 from ..discovery.apiserver import ApiServer
 from ...core.types import Hunter, ActiveHunter, KubernetesCluster, RemoteCodeExec, AccessRisk, InformationDisclosure, \
-    PrivilegeEscalation, DenialOfService
+    PrivilegeEscalation, DenialOfService, UnauthenticatedAccess
 
 """ Vulnerabilities """
 global final_pods
@@ -32,10 +32,34 @@ class ServerApiVersionEndPointAccessDos(Vulnerability, Event):
         Vulnerability.__init__(self, KubernetesCluster, name="Denial of Service to Kubernetes API Server", category=DenialOfService)
         self.evidence = evidence[37:60]
 
+class CheckCVE20181002101(Vulnerability, Event):
+    """CVE-2018-1002101"""
+    def __init__(self, evidence):
+        Vulnerability.__init__(self, KubernetesCluster, name="lead to command line argument injection", category=RemoteCodeExec)
+        self.evidence = evidence[37:60]
+
+class CheckCVE20199946(Vulnerability, Event):
+    """CVE-2019-9946"""
+    def __init__(self, evidence):
+        Vulnerability.__init__(self, KubernetesCluster, name="Unauthenticated Access", category=UnauthenticatedAccess)
+        self.evidence = evidence[37:60]
+
+class CheckCVE201911243(Vulnerability, Event):
+    """CVE-2019-11243"""
+    def __init__(self, evidence):
+        Vulnerability.__init__(self, KubernetesCluster, name="remains service account credentials", category=InformationDisclosure)
+        self.evidence = evidence[37:60]
+
+class CheckCVE201911244(Vulnerability, Event):
+    """CVE-2019-11244"""
+    def __init__(self, evidence):
+        Vulnerability.__init__(self, KubernetesCluster, name="--cache-dir may be modified by other users/groups", category=UnauthenticatedAccess)
+        self.evidence = evidence[37:60]
+
 class CheckCVE20191002101(Vulnerability, Event):
     """CVE-2019-1002101"""
     def __init__(self,evidence):
-        Vulnerability.__init__(self, KubernetesCluster, name="lead to command line argument injection", category=RemoteCodeExec)
+        Vulnerability.__init__(self, KubernetesCluster, name="run any code and output unexpected, malicious results", category=RemoteCodeExec)
         self.evidence = "* vulnerable pods * \n" + evidence
 
 
@@ -132,6 +156,69 @@ class IsVulnerableToCVEAttack(Hunter):
             check_lock.release()
             return False
 
+    def check_cve_2018_1002101(self, api_version):
+        first_two_minor_digists = api_version[0]
+        last_two_minor_digists = api_version[1]
+
+        if first_two_minor_digists == 11 and last_two_minor_digists < 9:
+            return True
+        elif first_two_minor_digists == 12 and last_two_minor_digists < 7:
+            return True
+        elif first_two_minor_digists == 13 and last_two_minor_digists < 4:
+            return True
+        elif first_two_manor_digists == 14 and last_two_minor_digists < 1:
+            return True
+        elif first_two_minor_digists < 11:
+            return True
+
+        return False
+
+    def check_cve_2019_9946(self, api_version):
+        first_two_minor_digists = api_version[0]
+        last_two_minor_digists = api_version[1]
+
+        if first_two_minor_digists == 11 and last_two_minor_digists < 9:
+            return True
+        elif first_two_minor_digists == 12 and last_two_minor_digists < 6:
+            return True
+        elif first_two_minor_digists == 13: 
+            if last_two_minor_digists < 4 or last_two_minor_digists == 6:
+                return True
+        elif first_two_manor_digists == 14 and last_two_minor_digists < 1:
+            return True
+        elif first_two_minor_digists < 11:
+            return True
+
+        return False
+
+    def check_cve_2019_11243(self, api_version):
+        first_two_minor_digists = api_version[0]
+        last_two_minor_digists = api_version[1]
+
+        if first_two_minor_digists == 12 and last_two_minor_digists < 5:
+            return True
+        elif first_two_minor_digists == 13 and last_two_minor_digists == 0:
+            return True
+        
+        return False
+
+    def check_cve_2019_11244(self, api_version):
+        first_two_minor_digists = api_version[0]
+        last_two_minor_digists = api_version[1]
+
+        if first_two_minor_digists == 11 and last_two_minor_digists < 10:
+            return True
+        elif first_two_minor_digists == 12 and last_two_minor_digists < 7:
+            return True
+        elif first_two_minor_digists == 13 and last_two_minor_digists < 4:
+            return True
+        elif first_two_manor_digists == 14 and last_two_minor_digists < 1:
+            return True
+        elif first_two_minor_digists < 11 and first_two_minor_digists > 7:
+            return True
+
+        return False
+
     def execute(self):
         api_version = self.get_api_server_version_end_point()
         global final_pods
@@ -139,8 +226,17 @@ class IsVulnerableToCVEAttack(Hunter):
             if self.check_cve_2019_1002101():
             	self.publish_event(CheckCVE20191002101(final_pods))
                 final_pods = ''
+            if self.check_cve_2019_9946(api_version):
+                self.publish_event(CheckCVE20199946(self.api_server_evidence))
+            if self.check_cve_2019_11243(api_version):
+                self.publish_event(CheckCVE201911243(self.api_server_evidence))
+            if self.check_cve_2019_11244(api_version):
+                self.publish_event(CheckCVE201911244(self.api_server_evidence))
+            if self.check_cve_2018_1002101(api_version):
+                self.publish_event(CheckCVE20181002101(self.api_server_evidence))
             if self.check_cve_2018_1002105(api_version):
                 self.publish_event(ServerApiVersionEndPointAccessPE(self.api_server_evidence))
             if self.check_cve_2019_1002100(api_version):
                 self.publish_event(ServerApiVersionEndPointAccessDos(self.api_server_evidence))
+        else: return
 
