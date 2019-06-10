caeda30e62b2efcdf0f965914d10b6ca923540ba
pu1et <46569798+pu1et@users.noreply.github.com>
2019-06-11 02:33:26 +0900

    Update Kube-Six.py

--- Kube-Six.py
+++ Kube-Six.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 from __future__ import print_function
-
+import requests
 import logging
 import threading
-
-try:
+try:    
     raw_input          # Python 2
 except NameError:
     raw_input = input  # Python 3
@@ -27,6 +26,15 @@ from src.modules.hunting.certificates import CertificateDiscovery
 from src.modules.hunting.cvehunter import IsVulnerableToCVEAttack
 import src
 
+class Email:
+    def __init__(self):
+        self.email = ''
+    def get_email(self):
+        return self.email
+    def set_email(self,email):
+        self.email = email
+
+email = Email()
 
 global hunt_started_lock
 hunt_started_lock = threading.Lock()
@@ -35,33 +43,48 @@ hunt_started = False
 
 def main():
     global hunt_started
-    try:
-        print("Hi, Kube-Six!")
-        print("Scans security weaknesses in Kubernetes clusters!")
-        hunt_started_lock.acquire()
-        hunt_started = True
-        hunt_started_lock.release()
-
-        handler.publish_event(HostScanEvent())
-        handler.join()
+    URL = "http://hotsix.kro.kr/re_result.php" 
+    print("\x1b[1;34m==========================================================\x1b[1;m")
+    print("\x1b[1;34mHi, Kube-Six!\x1b[1;m")
+    print("\x1b[1;34mKube-Six scans security weaknesses in Kubernetes clusters!\x1b[1;m")
+    print("\x1b[1;34m==========================================================\n\x1b[1;m")
+    print("\x1b[1;34mwrite your email (ex. user@google.com)\x1b[1;m")
         
-    except KeyboardInterrupt:
-        logging.debug("Kube-Hunter stopped by user")
-    except EOFError:
-        logging.error("\033[0;31mPlease run again with -it\033[0m")
-    finally:
-        hunt_started_lock.acquire()
-        if hunt_started:
-            hunt_started_lock.release()
-            handler.publish_event(HuntReported())
-            handler.join()
-            handler.free()
-            logging.debug("Cleaned Queue")
-        else:
-            hunt_started_lock.release()
+    USER_TOKEN = raw_input("\x1b[1;34mMy Email: \x1b[1;m")
+    if not "@" in USER_TOKEN:
+        USER_TOKEN = USER_TOKEN.split("@")[0]
+        print("check your email form:)")
+        return
+    else:
+        email.set_email(USER_TOKEN)
+        res = requests.post(URL, data={'chk':'0', 'token': USER_TOKEN})
+        if not "1" in res.text:
+            print("\x1b[1;34mThis email already exists.\n Please Try again.\x1b[1;m")
+            return
+        else :
+            try :
+                hunt_started_lock.acquire()
+                hunt_started = True
+                hunt_started_lock.release()
 
-
+                handler.publish_event(HostScanEvent())
+        
+                handler.join()
+            except KeyboardInterrupt:
+                logging.debug("Kube-Hunter stopped by user")
+            except EOFError:
+                logging.error("\033[0;31mPlease run again with -it\033[0m")
+            finally:
+                hunt_started_lock.acquire()
+                if hunt_started:
+                    hunt_started_lock.release()
+                    handler.publish_event(HuntReported())
+                    handler.join()
+                    handler.free()
+                    logging.debug("Cleaned Queue")
+                else:
+                    hunt_started_lock.release()
+                        
 
 if __name__ == '__main__':
         main()
-
